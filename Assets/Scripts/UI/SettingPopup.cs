7b359565545756468c0aa6d0f380e969640edbe7
ArHiFranki <franki550@gmail.com>
2021-11-24 18:13:45 +0700

    Добавил обработку дополнительных элементов UI

--- Assets/Scripts/UI/SettingPopup.cs
+++ Assets/Scripts/UI/SettingPopup.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingPopup : MonoBehaviour
 {
+    [SerializeField] private Slider _speedSlider;
+
+    private void Start()
+    {
+        _speedSlider.value = PlayerPrefs.GetFloat("speed", 1);
+    }
+
     public void Open()
     {
         gameObject.SetActive(true);
@@ -12,5 +20,16 @@ public class SettingPopup : MonoBehaviour
     public void Close()
     {
         gameObject.SetActive(false);
+    }
+
+    public void OnSubmitName(string name)
+    {
+        Debug.Log(name);
+    }
+
+    public void OnSpeedValue(float speed)
+    {
+        PlayerPrefs.SetFloat("speed", speed);
+        Debug.Log("Speed: " + speed);
     }
 }
