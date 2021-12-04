6aac1f8b1dd3f0c9024638ea3b6122f6618cda4b
ArHiFranki <franki550@gmail.com>
2021-12-05 03:47:32 +0700

    Добавил функцию HTML запросов

--- Assets/Scripts/Network/NetwokrService.cs
+++ Assets/Scripts/Network/NetwokrService.cs
@@ -1,8 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using System;
 
 public class NetwokrService
 {
-    
+    private const string xmlApi = 
+    "http://api.openweathermap.org/data/2.5/weather?q=Novosibirsk,ru&mode=xml&APPID=e1e8d773b4bbc8614c2bcf707372a4e5";
+
+    private IEnumerator CallAPI(string url, Action<string> callback )
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            yield return request.Send();
+
+            if (request.isNetworkError)
+            {
+                Debug.LogError("Network problem: " + request.error);
+            }
+            else if (request.responseCode != (long)System.Net.HttpStatusCode.OK)
+            {
+                Debug.LogError("Response error: " + request.responseCode);
+            }
+            else
+            {
+                callback(request.downloadHandler.text);
+            }
+        }
+    }
+
+    public IEnumerator GetWeatherXML(Action<string> callback)
+    {
+        return CallAPI(xmlApi, callback);
+    }
 }
