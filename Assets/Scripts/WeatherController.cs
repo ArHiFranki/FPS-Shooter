6365a132756275dc707bff400d5b05b247af9870
ArHiFranki <franki550@gmail.com>
2021-12-06 15:40:37 +0700

    Добавил изменение параметров сцены на основе полученных JSON-данных

--- Assets/Scripts/WeatherController.cs
+++ Assets/Scripts/WeatherController.cs
@@ -11,24 +11,32 @@ public class WeatherController : MonoBehaviour
     private float _cloudValue = 0f;
     private float _cluudValueMax = 1f;
 
+    private void Awake()
+    {
+        Messenger.AddListener(GameEvent.WEATHER_UPDATE, OnWeatherUpdated);
+    }
+
+    private void OnDestroy()
+    {
+        Messenger.RemoveListener(GameEvent.WEATHER_UPDATE, OnWeatherUpdated);
+    }
+
     private void Start()
     {
         _fullIntensity = _sunLight.intensity;
-    }
-
-    private void Update()
-    {
-        SetOvercast(_cloudValue);
-
-        if (_cloudValue < _cluudValueMax)
-        {
-            _cloudValue += 0.005f;
-        }
     }
 
     private void SetOvercast(float value)
     {
         _skyMaterial.SetFloat("_Blend", value);
         _sunLight.intensity = _fullIntensity - (_fullIntensity * value);
+    }
+
+    private void OnWeatherUpdated()
+    {
+        if (_cloudValue < _cluudValueMax)
+        {
+            SetOvercast(Managers.Weather.cloudValue);
+        }
     }
 }
