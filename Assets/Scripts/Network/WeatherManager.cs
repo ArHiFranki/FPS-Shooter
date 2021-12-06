d07333b03ee5e3dbdb99c1b97a02c2fe80724b63
ArHiFranki <franki550@gmail.com>
2021-12-06 14:28:43 +0700

    Добавил парсинг полученных XML-данных с рассылкой результатов через широковещательный ивент

--- Assets/Scripts/Network/WeatherManager.cs
+++ Assets/Scripts/Network/WeatherManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Xml;
+using System;
 
 public class WeatherManager : MonoBehaviour, IGameManager
 {
     public ManagerStatus status { get; private set; }
+    public float cloudValue { get; private set; }
 
     private NetwokrService _network;
 
@@ -20,7 +23,16 @@ public class WeatherManager : MonoBehaviour, IGameManager
 
     public void OnXMLDataLoaded(string data)
     {
-        Debug.Log(data);
+        XmlDocument document = new XmlDocument();
+        document.LoadXml(data);
+        XmlNode root = document.DocumentElement;
+
+        XmlNode node = root.SelectSingleNode("clouds");
+        string value = node.Attributes["value"].Value;
+        cloudValue = Convert.ToInt32(value) / 100f;
+        Debug.Log("Value: " + cloudValue);
+
+        Messenger.Broadcast(GameEvent.WEATHER_UPDATE);
 
         status = ManagerStatus.Started;
     }
