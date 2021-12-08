c3a819f68d43405f8a06c3b138a8be624de8af83
ArHiFranki <franki550@gmail.com>
2021-12-08 16:55:25 +0700

    Добавил микширование музыки при переключении фоновой музыки

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -6,9 +6,12 @@ public class AudioManager : MonoBehaviour, IGameManager
 {
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private AudioSource _music1Source;
+    [SerializeField] private AudioSource _music2Source;
 
     [SerializeField] private string _introBGMusic;
     [SerializeField] private string _levelBGMusic;
+
+    [SerializeField] private float _crossFadeRate = 1.5f;
 
     public ManagerStatus status { get; private set; }
     public float soundVolume
@@ -22,9 +25,10 @@ public class AudioManager : MonoBehaviour, IGameManager
         set 
         {
             _musicVolume = value;
-            if (_music1Source != null)
+            if (_music1Source != null && !_crossFading)
             {
                 _music1Source.volume = _musicVolume;
+                _music2Source.volume = _musicVolume;
             }
         }
     }
@@ -48,12 +52,16 @@ public class AudioManager : MonoBehaviour, IGameManager
             if (_music1Source != null)
             {
                 _music1Source.mute = value;
+                _music2Source.mute = value;
             }
         }
     }
 
     private NetwokrService _network;
+    private AudioSource _activeMusic;
+    private AudioSource _inactiveMusic;
     private float _musicVolume;
+    private bool _crossFading;
 
     public void Startup(NetwokrService service)
     {
@@ -61,10 +69,15 @@ public class AudioManager : MonoBehaviour, IGameManager
         _network = service;
 
         _music1Source.ignoreListenerVolume = true;
+        _music2Source.ignoreListenerVolume = true;
         _music1Source.ignoreListenerPause = true;
+        _music2Source.ignoreListenerPause = true;
 
         soundVolume = 1f;
         musicVolume = 1f;
+
+        _activeMusic = _music1Source;
+        _inactiveMusic = _music2Source;
 
         status = ManagerStatus.Started;
     }
@@ -86,12 +99,40 @@ public class AudioManager : MonoBehaviour, IGameManager
 
     private void PlayMusic(AudioClip clip)
     {
-        _music1Source.clip = clip;
-        _music1Source.Play();
+        if (_crossFading) { return; }
+        StartCoroutine(CrossFadeMusic(clip));
     }
 
     public void StopMusic()
     {
-        _music1Source.Stop();
+        _activeMusic.Stop();
+        _inactiveMusic.Stop();
+    }
+
+    private IEnumerator CrossFadeMusic(AudioClip clip)
+    {
+        _crossFading = true;
+
+        _inactiveMusic.clip = clip;
+        _inactiveMusic.volume = 0;
+        _inactiveMusic.Play();
+
+        float scaleRate = _crossFadeRate * _musicVolume;
+        while (_activeMusic.volume > 0)
+        {
+            _activeMusic.volume -= scaleRate * Time.deltaTime;
+            _inactiveMusic.volume += scaleRate * Time.deltaTime;
+
+            yield return null;
+        }
+
+        AudioSource temp = _activeMusic;
+
+        _activeMusic = _inactiveMusic;
+        _activeMusic.volume = _musicVolume;
+
+        _inactiveMusic = temp;
+        _inactiveMusic.Stop();
+        _crossFading = false;
     }
 }
